f4da57a75aa816bd704e18ea95f7bc88818b23d2
Sujal Phaiju <sujalphaiju777@gmail.com>
2025-09-23 16:58:03 +0545

    multi route table updates

--- modules/route_tables/main.tf
+++ modules/route_tables/main.tf
@@ -1,64 +1,88 @@
 variable "vpc_id" {}
 variable "igw_id" {}
 variable "nat_gateway_id" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
+variable "public_subnets" {
+  description = "List of public subnet objects with id and availability_zone"
+  type = list(object({
+    id                = string
+    availability_zone = string
+  }))
+}
+variable "private_subnets" {
+  description = "List of private subnet objects with id and availability_zone"
+  type = list(object({
+    id                = string
+    availability_zone = string
+  }))
+}
 variable "tags" { type = map(string) }
 
-
-# Public Route Table
-resource "aws_route_table" "public" {
-vpc_id = var.vpc_id
-
-
-route {
-cidr_block = "0.0.0.0/0"
-gateway_id = var.igw_id
-}
-
-
-tags = merge(var.tags, { Name = "public-rt" })
-}
-
-resource "aws_route_table_association" "public" {
-  for_each = { for idx, subnet in var.public_subnets : "public-${idx}" => subnet }
-  subnet_id      = each.value
-  route_table_id = aws_route_table.public.id
-}
-
-
-# Private Route Table
 variable "create_nat" {
   type    = bool
   default = false
 }
 
-resource "aws_route_table" "private" {
+# Public Route Tables (per AZ)
+resource "aws_route_table" "public" {
+  for_each = {
+    for s in var.public_subnets : s.availability_zone => s
+  }
+
   vpc_id = var.vpc_id
 
-  tags = merge(var.tags, { Name = "private-rt" })
+  tags = merge(var.tags, { Name = "public-rt-${each.key}" })
 }
 
-resource "aws_route" "private_nat_route" {
-  count                  = var.create_nat && var.nat_gateway_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private.id
+resource "aws_route" "public_igw" {
+  for_each = aws_route_table.public
+
+  route_table_id         = each.value.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = var.igw_id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = {
+    for s in var.public_subnets : s.id => s.availability_zone
+  }
+
+  subnet_id      = each.key
+  route_table_id = aws_route_table.public[each.value].id
+}
+
+# Private Route Tables (per AZ)
+resource "aws_route_table" "private" {
+  for_each = {
+    for s in var.private_subnets : s.availability_zone => s
+  }
+
+  vpc_id = var.vpc_id
+
+  tags = merge(var.tags, { Name = "private-rt-${each.key}" })
+}
+
+resource "aws_route" "private_nat" {
+  for_each = var.create_nat && var.nat_gateway_id != null ? aws_route_table.private : {}
+
+  route_table_id         = each.value.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = var.nat_gateway_id
 }
 
-
 resource "aws_route_table_association" "private" {
-  for_each = { for idx, subnet in var.private_subnets : "private-${idx}" => subnet }
-  subnet_id      = each.value
-  route_table_id = aws_route_table.private.id
+  for_each = {
+    for s in var.private_subnets : s.id => s.availability_zone
+  }
+
+  subnet_id      = each.key
+  route_table_id = aws_route_table.private[each.value].id
 }
 
-
-output "public_rt_id" {
-value = aws_route_table.public.id
+# Outputs
+output "public_rt_ids" {
+  value = { for k, rt in aws_route_table.public : k => rt.id }
 }
 
-
-output "private_rt_id" {
-value = aws_route_table.private.id
+output "private_rt_ids" {
+  value = { for k, rt in aws_route_table.private : k => rt.id }
 }
